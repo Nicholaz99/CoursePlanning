1e511d9932850e9dcdbe39997db27bd26edb5dab
Nicholas R. Putra <nicholasmagbanua@gmail.com>
2018-03-18 21:36:54 +0700

    Huyu

---
+++ CoursePlanning/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Tubes2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SidePanel = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.SidePanel);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(244, 666);
+            this.panel1.TabIndex = 0;
+            // 
+            // SidePanel
+            // 
+            this.SidePanel.AccessibleName = "";
+            this.SidePanel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.SidePanel.Location = new System.Drawing.Point(0, 101);
+            this.SidePanel.Name = "SidePanel";
+            this.SidePanel.Size = new System.Drawing.Size(20, 87);
+            this.SidePanel.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(0, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(244, 87);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "About Us";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(0, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 87);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BFS";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button4.Location = new System.Drawing.Point(0, 185);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(244, 87);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "DFS";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 29F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(988, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(358, 75);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Run the code";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(261, 600);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 57);
+            this.label2.TabIndex = 3;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(285, 35);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(358, 75);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Show the Graph";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 29F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(988, 36);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(358, 75);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Run the code";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Open Sans Semibold", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(663, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 68);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Created by :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Open Sans Semibold", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(414, 453);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(829, 204);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nicholas Rianto Putra - 13516020\r\nAbram Perdanaputra - 13516083\r\nFaza Fahleraz - " +
+    "13516095";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(239, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1147, 663);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1385, 666);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel SidePanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CoursePlanning/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tubes2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Visible = false;
+            button3.Visible = false;
+            label3.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Graph g = new Graph("./input/input.txt");
+            string ans = "";
+            g.topologicalSort(ref ans, "dfs");
+            label2.Text = "Answer with DFS = " + ans;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SidePanel.Height = button1.Height;
+            SidePanel.Top = button1.Top;
+            button3.Visible = false;
+            button6.Visible = true;
+            label2.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SidePanel.Height = button2.Height;
+            SidePanel.Top = button2.Top;
+            button6.Visible = false;
+            button3.Visible = false;
+            button5.Visible = false;
+            label2.Text = "";
+            label1.Visible = true;
+            label3.Visible = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SidePanel.Height = button4.Height;
+            SidePanel.Top = button4.Top;
+            button6.Visible = false;
+            button3.Visible = true;
+            label2.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Graph g = new Graph("./input/input.txt");
+            g.initGraph();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Graph g = new Graph("./input/input.txt");
+            string ans = "";
+            g.topologicalSort(ref ans, "bfs");
+            label2.Text = "Answer with BFS = " + ans;
+        }
+    }
+
+    class Graph
+    {
+        private Dictionary<string, HashSet<string>> graph;
+        private Dictionary<string, bool> visited = new Dictionary<string, bool>();
+        private int no_of_vertex;
+
+        public Graph(string file)
+        {
+            // Read input.txt and store to matrix
+            string input = System.IO.File.ReadAllText(file);
+            string[] lines = input.Split('\n');
+            List<string>[] matrix = new List<string>[lines.Length];
+
+            // Initialize all the list in matrix
+            for (int i = 0; i < lines.Length; i++)
+            {
+                matrix[i] = new List<string>();
+            }
+
+            // Add each element to list
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] temp = lines[i].Remove(lines[i].Length - 1).Split(',');
+                foreach (string s in temp)
+                {
+                    matrix[i].Add(s);
+                }
+            }
+
+            no_of_vertex = matrix.Length;
+
+            graph = new Dictionary<string, HashSet<string>>(); // Initialize matrix to zeros
+            for (int i = 0; i < no_of_vertex; i++)
+            {
+                graph[matrix[i][0]] = new HashSet<string>();
+                for (int j = 1; j < matrix[i].Count; j++)
+                {
+                    graph[matrix[i][0]].Add(matrix[i][j]);
+                }
+            }
+        }
+
+        private void initializeVisited()
+        {
+            foreach (string s in graph.Keys)
+            {
+                visited[s] = false;
+            }
+        }
+
+        private bool isAdjacent(string src, string dest)
+        {
+            return graph[dest].Contains(src);
+        }
+
+        private void deleteEdge(string src, string dest)
+        {
+            graph[dest].Remove(src);
+        }
+
+        private bool visitedAllVertex()
+        {
+            bool visited_all = true;
+            foreach (string i in graph.Keys)
+            {
+                if (!visited[i])
+                {
+                    visited_all = false;
+                    break;
+                }
+            }
+            return visited_all;
+        }
+
+        private int compareTuple(Tuple<int, string> x, Tuple<int, string> y)
+        {
+            return x.Item1 > y.Item1 ? -1 : 1;
+        }
+
+        private string vertexNoIn()
+        {
+            string res = "\0";
+            foreach (KeyValuePair<string, HashSet<string>> kvp in graph)
+            {
+                if (kvp.Value.Count == 0)
+                {
+                    res = kvp.Key;
+                    break;
+                }
+            }
+            return res;
+        }
+
+        private bool noOutEdge(string v)
+        {
+            bool no_out_edge = true;
+            foreach (string i in graph.Keys)
+            {
+                if (isAdjacent(v, i))
+                {
+                    no_out_edge = false;
+                    break;
+                }
+            }
+            return no_out_edge;
+        }
+
+        private bool noInEdge(string v)
+        {
+            bool no_in_edge = true;
+            foreach (string i in graph.Keys)
+            {
+                if (isAdjacent(i, v))
+                {
+                    no_in_edge = false;
+                    break;
+                }
+            }
+            return no_in_edge;
+        }
+
+        private void DFS(string vertex, ref List<Tuple<int, string>> list, ref int timestamp, ref List<string> vertexes)
+        {
+            visited[vertex] = true;
+            vertexes.Add(vertex);
+            
+            // Start timestamp
+            list.Add(Tuple.Create(timestamp, vertex));
+            timestamp++;
+            foreach (string i in graph.Keys)
+            {
+                if (isAdjacent(vertex, i) && !visited[i])
+                {
+                    DFS(i, ref list, ref timestamp, ref vertexes);
+                }
+            }
+            // Finished timestamp
+            list.Add(Tuple.Create(timestamp, vertex));
+            timestamp++;
+        }
+
+        public void initGraph()
+        {
+            Form form = new Form();
+            //create a viewer object 
+            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            //create a graph object 
+            Microsoft.Msagl.Drawing.Graph graf = new Microsoft.Msagl.Drawing.Graph("graf");
+            graf.Attr.LayerDirection = Microsoft.Msagl.Drawing.LayerDirection.LR;
+            //create the graph content
+            foreach (KeyValuePair<string, HashSet<string>> kvp in graph)
+            {
+                foreach (string j in kvp.Value)
+                {
+                    graf.AddEdge(j, kvp.Key);
+                }
+            }
+            viewer.Graph = graf;
+            //associate the viewer with the form 
+            form.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            form.Controls.Add(viewer);
+            form.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            form.StartPosition = FormStartPosition.Manual;
+            form.Location = new Point(444, 300);
+            form.Size = new Size(830, 300);
+            form.ResumeLayout();
+            form.BringToFront();
+            form.Show();
+        }
+
+        private void BFS(string vertex, ref List<string> list, ref List<string> vertexes)
+        {
+            Queue<string> q = new Queue<string>();
+            q.Enqueue(vertex);
+            while ((q.Count != 0) && !visitedAllVertex())
+            {
+                string v = q.Dequeue();
+                if (noInEdge(v) && !visited[v])
+                {
+                    visited[v] = true;
+                    vertexes.Add(v);
+                    foreach (string i in graph.Keys)
+                    {
+                        if (isAdjacent(v, i) && !visited[i])
+                        {
+                            deleteEdge(v, i);
+                            q.Enqueue(i);
+                        }
+                    }
+                    list.Add(v);
+                }
+            }
+        }
+
+        public void topologicalSort(ref string ans, string chooseRoute)
+        {
+            Form form = new Form();
+            //create a viewer object 
+            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            //create a graph object 
+            Microsoft.Msagl.Drawing.Graph graf = new Microsoft.Msagl.Drawing.Graph("graf");
+            //create the graph content
+            graf.Attr.LayerDirection = Microsoft.Msagl.Drawing.LayerDirection.LR;
+            foreach (KeyValuePair<string, HashSet<string>> kvp in graph)
+            {
+                foreach (string j in kvp.Value)
+                {
+                    graf.AddEdge(j, kvp.Key);
+                }
+            }
+            viewer.Graph = graf;
+            //associate the viewer with the form 
+            form.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            form.Controls.Add(viewer);
+            form.StartPosition = FormStartPosition.Manual;
+            form.Location = new Point(444, 300);
+            form.Size = new Size(830, 300);
+            form.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            form.ResumeLayout();
+            form.Show();
+
+            // Tuple of timestamp and vertex
+           
+            if (chooseRoute == "dfs")
+            {
+                List<Tuple<int, string>> listDFS = new List<Tuple<int, string>>();
+                initializeVisited();
+                int timestamp = 1;
+                List<string> vertexes = new List<string>();
+                DFS(vertexNoIn(), ref listDFS, ref timestamp, ref vertexes);
+                for (int i = 0; i < vertexes.Count; i++)
+                {
+                    //form.Close();
+                    graf.FindNode(vertexes[i]).Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+                    form.Refresh();
+                    var t = Task.Delay(1000);
+                    t.Wait();
+                }
+                listDFS.Sort(compareTuple);
+
+                // Print all ordered
+                string temp = "";
+                HashSet<string> printed = new HashSet<string>();
+                for (int i = 0; i < listDFS.Count; i++)
+                {
+                    if (!printed.Contains(listDFS[i].Item2))
+                    {
+                        printed.Add(listDFS[i].Item2);
+                        temp = temp + (listDFS[i].Item2) + " ";
+                    }
+                }
+                ans = temp;
+            }
+            
+            if (chooseRoute == "bfs")
+            {
+                List<string> listBFS = new List<string>();
+                initializeVisited();
+                List<string> vertexes = new List<string>();
+                BFS(vertexNoIn(), ref listBFS, ref vertexes);
+                for (int i = 0; i < vertexes.Count; i++)
+                {
+                    //form.Close();
+                    graf.FindNode(vertexes[i]).Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+                    form.Refresh();
+                    var t = Task.Delay(1000);
+                    t.Wait();
+                }
+
+                // Print all Ordered
+                string temp = "";
+                for (int i = 0; i < listBFS.Count; i++)
+                {
+                    temp = temp + (listBFS[i] + " ");
+                }
+                ans = temp;
+            }
+        }
+    }
+}
+
